25a4e6dd79a0920f8be17fbd07d08d616e5a1c59
muhammad6153 <owais6153@gmail.com>
2024-01-11 04:09:21 +0500

    Divide tests into suits

--- tests/integration/auth/login.test.js
+++ tests/integration/auth/login.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest')
-const app = require('../../app')
-const { API_PREFIX } = require('../../config/app')
-const { sequelize } = require('../../models')
-const UserRepository = require('../../repositories/user')
+const app = require('../../../app')
+const { API_PREFIX } = require('../../../config/app')
+const { sequelize } = require('../../../models')
+const UserRepository = require('../../../repositories/user')
 
 const baseRoute = API_PREFIX + '/auth'
 
@@ -18,40 +18,12 @@ var authToken = ''
 beforeAll(async () => {
   try {
     await sequelize.authenticate()
+    const res = await request(app).post(`${baseRoute}/signup`).send(data)
+    authToken = res?.body?.data?.email
   } catch (err) {
     console.error('Unable to connect to the database:', err)
     process.exit(1)
   }
-})
-
-describe(`POST ${baseRoute}/signup`, () => {
-  it('Should return 400 for empty fields', () => {
-    return request(app).post(`${baseRoute}/signup`).expect(400)
-  })
-  it('Should return 400 for missing fields', () => {
-    return request(app)
-      .post(`${baseRoute}/signup`)
-      .send({
-        email: data.email,
-        password: data.password,
-      })
-      .expect(400)
-  })
-  it('Should return 400 for email already exsists', () => {
-    return request(app)
-      .post(`${baseRoute}/signup`)
-      .send({
-        email: 'admin@admin.com',
-        password: data.password,
-      })
-      .expect(400)
-  })
-  it('Should return 200 with authToken after successful signup', async () => {
-    const res = await request(app).post(`${baseRoute}/signup`).send(data)
-
-    expect(res.statusCode).toBe(200)
-    expect(res.body.data.email).toBe(data.email)
-  })
 })
 
 describe(`POST ${baseRoute}/authenticate`, () => {
@@ -67,12 +39,11 @@ describe(`POST ${baseRoute}/authenticate`, () => {
       })
       .expect(400)
   })
-  it('Should return 200 for correct email and password', async () => {
+  it('Should return 200 user object for correct email and password', async () => {
     const res = await request(app).post(`${baseRoute}/authenticate`).send({
       email: data.email,
       password: data.password,
     })
-    authToken = res.body.data.authToken
     expect(res.statusCode).toBe(200)
     expect(res.body.data.email).toBe(data.email)
   })
@@ -89,10 +60,10 @@ describe(`GET ${baseRoute}/me`, () => {
         email: 'wrongemail@test.com',
         password: 'secret123',
       })
-      .set('x-auth-token', authToken + 'wrong')
+      .set('x-auth-token', authToken + 'invalid')
       .expect(400)
   })
-  it('Should return 200 for correct authToken', async () => {
+  it('Should return 200 and user object', async () => {
     const res = await request(app)
       .get(`${baseRoute}/me`)
       .send({
---
+++ tests/integration/auth/signup.test.js
@@ -0,0 +1,58 @@
+const request = require('supertest')
+const app = require('../../../app')
+const { API_PREFIX } = require('../../../config/app')
+const { sequelize } = require('../../../models')
+const UserRepository = require('../../../repositories/user')
+
+const baseRoute = API_PREFIX + '/auth'
+
+const data = {
+  email: 'fakeemail@test.com',
+  password: 'secret123',
+  firstName: 'First',
+  lastName: 'Last',
+}
+
+beforeAll(async () => {
+  try {
+    await sequelize.authenticate()
+  } catch (err) {
+    console.error('Unable to connect to the database:', err)
+    process.exit(1)
+  }
+})
+
+describe(`POST ${baseRoute}/signup`, () => {
+  it('Should return 400 for empty fields', () => {
+    return request(app).post(`${baseRoute}/signup`).expect(400)
+  })
+  it('Should return 400 for missing fields', () => {
+    return request(app)
+      .post(`${baseRoute}/signup`)
+      .send({
+        email: data.email,
+        password: data.password,
+      })
+      .expect(400)
+  })
+  it('Should return 400 for email already exsists', () => {
+    return request(app)
+      .post(`${baseRoute}/signup`)
+      .send({
+        email: 'admin@admin.com',
+        password: data.password,
+      })
+      .expect(400)
+  })
+  it('Should return 200 with authToken after successful signup', async () => {
+    const res = await request(app).post(`${baseRoute}/signup`).send(data)
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body.data.email).toBe(data.email)
+  })
+})
+
+afterAll(async () => {
+  const userRepo = new UserRepository()
+  await userRepo.delete({ where: { email: data.email } })
+})
