0e16394ef880955bf346f38bbfe8f98896347b0b
muhammad6153 <owais6153@gmail.com>
2024-01-26 01:30:59 +0500

    Twilio Setup

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const authRoutes = require('./auth')
+const twilio = require('../../util/twilio')
 
 router.use(`/auth`, authRoutes)
+
+//Send Mssage with Twilio
+router.get('/send-message', (req, res) => {
+  twilio.sendSMS('923410216406', 'Test Twilio')
+})
 
 module.exports = router
---
+++ util/twilio.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const twilio = require('../config/twilio')
+module.exports = {
+  sendSMS: (toPhoneNumber, message, shouldThrowError = false) => {
+    try {
+      console.log({
+        body: message,
+        from: process.env.TWILIO_FROM_PHONE,
+        to: `+${toPhoneNumber}`,
+      })
+
+      twilio.messages
+        .create({
+          body: message,
+          from: process.env.TWILIO_FROM_PHONE,
+          to: `+${toPhoneNumber}`,
+        })
+        .then(message => console.log('Message Sent: ', message))
+        .catch(err => {
+          if (shouldThrowError) throw new Error(err)
+          console.log('Error sending message from Twilio: ', err)
+        })
+    } catch (error) {
+      if (shouldThrowError) throw new Error(error)
+      console.log('Error sending message from twilio: ', error)
+    }
+  },
+}
