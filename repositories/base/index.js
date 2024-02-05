65dea4c4ba71785b81861071dc861400b4ca5fd1
muhammad6153 <owais6153@gmail.com>
2024-02-06 04:58:50 +0500

    Changes

--- repositories/base/index.js
+++ repositories/base/index.js
@@ -40,83 +40,57 @@ class BaseRepository extends RepositoryResponse {
     return this
   }
 
-  async create(data) {
+  async run(promise, shouldReturn = false) {
     try {
-      const entity = await this._model.create(
-        data,
-        this._transaction ? { transaction: this._transaction } : {}
-      )
-      this.setResponse(entity)
+      const resp = await promise()
+      if (!shouldReturn) this.setResponse(resp)
+      else return resp
     } catch (error) {
       this.setError(error)
     }
+  }
+
+  async create(data) {
+    await this.run(
+      async () =>
+        await this._model.create(data, this._transaction ? { transaction: this._transaction } : {})
+    )
   }
   async findOne(attributes = {}) {
-    try {
-      const entity = await this._model.findOne(attributes)
-      this.setResponse(entity)
-    } catch (error) {
-      this.setError(error)
-    }
+    await this.run(async () => await this._model.findOne(attributes))
   }
   async findAll(attributes = {}) {
-    try {
-      const entities = await this._model.findAll(attributes)
-      this.setResponse(entities)
-    } catch (error) {
-      this.setError(error)
-    }
+    await this.run(async () => await this._model.findAll(attributes))
   }
   async update(attributes = { where: { id: 0 } }, data) {
-    try {
-      const entities = await this._model.update(
-        data,
-        this._transaction ? { ...attributes, transaction: this._transaction } : attributes
-      )
-      this.setResponse(entities)
-    } catch (error) {
-      this.setError(error)
-    }
+    let attr = this._transaction ? { ...attributes, transaction: this._transaction } : attributes
+    await this.run(async () => await this._model.update(data, attr))
   }
   async delete(attributes = { where: { id: 0 } }) {
-    try {
-      const res = await this._model.destroy(
-        this._transaction ? { ...attributes, transaction: this._transaction } : attributes
-      )
-      this.setResponse(res)
-    } catch (error) {
-      this.setError(error)
-    }
+    let attr = this._transaction ? { ...attributes, transaction: this._transaction } : attributes
+    await this.run(async () => await this._model.destroy(attr))
   }
   async count(attributes = {}) {
-    try {
-      const res = await this._model.count(attributes)
-      this.setResponse(res)
-    } catch (error) {
-      this.setError(error)
-    }
+    await this.run(async () => await this._model.count(attributes))
   }
   async paginate(pageNumber, pageSize, attributes = {}) {
-    try {
-      const offset = (pageNumber - 1) * pageSize
-
-      const { count, rows } = await this._model.findAndCountAll({
-        offset: Math.max(offset, 0),
-        limit: pageSize,
-        ...attributes,
-      })
-
-      const totalPages = Math.ceil(count / pageSize)
-
-      this.setResponse({
-        rows,
-        totalCount: count,
-        totalPages,
-        currentPage: pageNumber,
-      })
-    } catch (error) {
-      this.setError(error)
-    }
+    const offset = (pageNumber - 1) * pageSize
+    const { count, rows } = await this.run(
+      async () =>
+        await this._model.findAndCountAll({
+          offset: Math.max(offset, 0),
+          limit: pageSize,
+          ...attributes,
+        }),
+      true
+    )
+    const totalPages = Math.ceil(count / pageSize)
+    this.setResponse({
+      rows,
+      totalCount: count,
+      totalPages,
+      currentPage: pageNumber,
+    })
   }
 }
 
