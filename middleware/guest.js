96599d4d494f10494f81eb79186a6f46a760ae7e
muhammad6153 <owais6153@gmail.com>
2024-02-05 22:58:02 +0500

    Guest Middleware

---
+++ middleware/guest.js
@@ -0,0 +1,7 @@
+const { errorResponse } = require('../util/responses')
+
+module.exports = function (req, res, next) {
+  const token = req.header('x-auth-token')
+  if (!token) next()
+  else return errorResponse(res, 'Already Authenticated', 406)
+}
--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const authController = require('../../controllers/auth')
 const isAuthenticated = require('../../middleware/auth')
+const isGuest = require('../../middleware/guest')
 
-router.post('/authenticate', authController.authenticate)
+router.post('/authenticate', isGuest, authController.authenticate)
 
-router.post('/signup', authController.signUp)
+router.post('/signup', isGuest, authController.signUp)
 
 router.get('/me', isAuthenticated, authController.me)
 
