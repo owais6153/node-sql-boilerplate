f6570c2c6d9d89e54b4fbac99195f25983a596db
M Owais Khan <46226315+owais6153@users.noreply.github.com>
2024-02-05 22:46:13 +0500

    Unauthorized Requests

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,13 +4,13 @@ const { errorResponse } = require('../util/responses')
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token')
   if (!token) {
-    return errorResponse(res, 'Unauthorized Request', 400)
+    return errorResponse(res, 'Unauthorized Request', 401)
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
     req.user = decoded
     next()
   } catch (error) {
-    return errorResponse(res, 'Authorization code is invalid', 400)
+    return errorResponse(res, 'Authorization code is invalid', 401)
   }
 }
